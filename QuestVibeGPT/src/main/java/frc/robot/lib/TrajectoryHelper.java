fc65f339f293e5364d21bf75ea420abd4ca6737d
Sudhamsu <academyoffice79@gmail.com>
2025-11-23 17:46:26 -0500

    Started AUTO-Selector

--- QuestVibeGPT/src/main/java/frc/robot/lib/TrajectoryHelper.java
+++ QuestVibeGPT/src/main/java/frc/robot/lib/TrajectoryHelper.java
@@ -1,11 +1,35 @@
 package frc.robot.lib;
 
+import java.util.ArrayList;
+
+import javax.xml.crypto.dsig.spec.ExcC14NParameterSpec;
+
+import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.FlippingUtil;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.Constants.PathPlannerConstants;
 
 public class TrajectoryHelper {
+
+    private static final double FIELD_LENGTH_BLUE = 8.2296; // 27 feet
+
+    private ArrayList<PathPlannerPath> blueTrajectories = new ArrayList<PathPlannerPath>();
+    private ArrayList<PathPlannerPath> redTrajectories = new ArrayList<PathPlannerPath>();
+
+    public TrajectoryHelper() {
+        try {
+            blueTrajectories.add(PathPlannerPath.fromPathFile("ElasticTestPath"));
+            blueTrajectories.add(PathPlannerPath.fromPathFile("ThreeMeterForward"));
+            blueTrajectories.add(PathPlannerPath.fromPathFile("OneMeterForwardTurn"));
+
+            redTrajectories.add(PathPlannerPath.fromPathFile("RedElasticTestPath"));
+            redTrajectories.add(PathPlannerPath.fromPathFile("RedThreeMeterForward"));
+            redTrajectories.add(PathPlannerPath.fromPathFile("RedOneMeterForwardTurn"));
+        } catch (Exception e) {
+            
+        }
+    }
 
     /**
      * When running a trajectory, PP flips it automatically (unless disabled by a flag)
@@ -21,5 +45,28 @@ public class TrajectoryHelper {
         return (PathPlannerConstants.shouldFlipTrajectoryOnRed) ? 
             FlippingUtil.flipFieldPose(pose) :
             pose;
+    }
+
+    public static PathPlannerPath selectAutoBasedOnATPose(Pose2d robotPose2d) {
+        boolean isRobotBlue = true;
+        if (robotPose2d.getX() < FIELD_LENGTH_BLUE / 2) {
+            isRobotBlue = true;
+        } else {
+            isRobotBlue = false;
+        }
+
+        if(isRobotBlue) {
+            return nearestTrajectoryBlue();
+        } else {
+            return nearestTrajectoryRed();
+        }
+    }
+
+    private static PathPlannerPath nearestTrajectoryBlue() {
+        return null;
+    }
+
+    private static PathPlannerPath nearestTrajectoryRed() {
+        return null;
     }
 }
